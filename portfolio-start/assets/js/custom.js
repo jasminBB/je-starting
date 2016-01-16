8f6c70f784d40bb276c2eecf580766f73bf309ba
Jasmin Besic <jasmin@jasmin-Aspire-ES1-512.(none)>
2016-01-17 00:44:17 +0100

    Tablet adjustment

--- portfolio-start/assets/js/custom.js
+++ portfolio-start/assets/js/custom.js
@@ -4,4 +4,20 @@ jQuery(document).ready(function(){
 		navigationPosition: "right",
 		scrollOverflow: true
 	});
+
+	/* Calculate viewport width */
+	var vWidth = jQuery(window).innerWidth();
+	jQuery(window).resize(function() {
+		vWidth = jQuery(window).innerWidth();
+		tabletDevices(vWidth);
+	});
+	tabletDevices(vWidth);
 });
+
+function tabletDevices(vWidth) {
+	if(vWidth >= 768 && vWidth < 992) {
+		jQuery("[data-device='tablet']").addClass("fp-auto-height");
+	} else  {
+		jQuery("[data-device='tablet']").removeClass("fp-auto-height");
+	}
+}
